9967c5a52720b7dd1c32f29838f5e6c92a56a6d4
Ahmed Kamel Taha <ahmedengu@gmail.com>
2016-10-01 00:17:36 +0200

    https

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var api = new ParseServer({
     restAPIKey: "master",
     clientKey: "master",
     masterKey: process.env.MASTER_KEY || 'master', //Add your master key here. Keep it secret!
-    serverURL: process.env.SERVER_URL || 'https://localhost:1337/parse',  // Don't forget to change to https if needed
+    serverURL: process.env.SERVER_URL || 'https://env-3406900.mircloud.host/parse',  // Don't forget to change to https if needed
     liveQuery: {
         classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
     },
@@ -54,7 +54,7 @@ app.get('/', function (req, res) {
 var dashboard = new ParseDashboard({
     "apps": [
         {
-            "serverURL": "https://localhost:1337/parse",
+            "serverURL": "https://env-3406900.mircloud.host/parse",
             "appId": "myAppId",
             "masterKey": "master",
             "appName": "MyApp"
@@ -73,6 +73,6 @@ httpServer.listen(port, function () {
 ParseServer.createLiveQueryServer(httpServer);
 
 
-// parse-dashboard --appId myAppId --masterKey master --serverURL "http://localhost:1337/parse"
+// parse-dashboard --appId myAppId --masterKey master --serverURL "https://env-3406900.mircloud.host/parse"
 
 
